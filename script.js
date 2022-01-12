3dd7a969dcd3f1aacd7a80e54d83fca17670646c
GlobalOffender <99067159@mydavinci.nl>
2022-01-12 11:13:25 +0100

    Working

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ btn.innerHTML = '...';
 document.body.appendChild(btn);
 
 // schijf hier tussen je code
-
-
-
+btn.onclick = function(){
+    lightswitch(); 
+}
+function lightswitch(){
+  if (btn.innerHTML == "switch light on"){
+    btn.innerHTML = "switch light off";
+    document.body.style.backgroundColor = "white";
+  } else {
+    btn.innerHTML = "switch light on";
+    document.body.style.backgroundColor = "black";
+  }
+}
 // schijf hier tussen je code
